381d12dc5c389c94d21ffae61a956bccc21178fe
JazHarlow <Jazreenaharlow@gmail.com>
2022-10-06 11:49:19 +0100

    added changes from class

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,12 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log ('Hello Everybody')
+
+console.log ('Hello Everybody was here')
+
+console.log('Inner HTML of 1st p:', document.querySelector('.about').innerHTML)
+
+console.log('description:', document.querySelector('.about'))
+console.log('ALL about', document.querySelectorAll('.about'))
+console.log('p', document.querySelectorAll('.about'))
+
