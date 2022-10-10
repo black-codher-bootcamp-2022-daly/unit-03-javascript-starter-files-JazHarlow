96cf2389bd83505e9f1d189d0b17fb40b02c6d6c
JazHarlow <Jazreenaharlow@gmail.com>
2022-10-10 19:02:44 +0100

    initial commit

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,16 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+let a = 5;
+let b = 10;
+
+console.log(a+b);
+
+a = 20;
+
+console.log(a+b);
+
+const c = a + b;
+
+console.log(c);
+
