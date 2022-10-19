0265618f0e37dd4fddbb0f953a9ca1024d513edb
JazHarlow <Jazreenaharlow@gmail.com>
2022-10-19 19:11:54 +0100

    added task 1

--- session-6/index.js
+++ session-6/index.js
@@ -1,10 +1,46 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
-const randomNumber = Math.round(Math.random() * 10);
+
+// let counter = 1;
+// while(counter <= 10) {
+// const randomNumber = Math.round(Math.random() * 10);
+
+//     console.log('while Random', randomNumber,)
+//     counter = counter + 1;
+//   }
+
+
+  let counter = 0;
+
+while (counter < 10) {
+    const randomNumber = Math.round(Math.random() * 10);
+    console.log('while random',randomNumber);    
+    counter = counter + 1
+}
+
+
 
 // ****************************************************TASK 1****************************************************
 
 
+//let counter = 0;
+//let totalShoes = 0;
 
+//while (counter < 6) {
+
+//totalShoes = totalShoes +1
+
+//if (totalShoes % 2 === 0) {
+  //  console.log('I have', totalShoes, 'pairs')
+//}
+
+    // do something
+   // console.log('inside the while', counter)
+    //counter = counter + 1
+  
+   
+//}
+
+// console.log('counter', counter)
 
 
 
